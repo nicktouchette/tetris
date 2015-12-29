417b3085081a792599d73a299d6cb65a54ee9e20
Nick Touchette <touchette@gmail.com>
2015-12-29 04:04:07 -0500

    Hacked in a next block preview, will refactor code next

--- js/tetris.js
+++ js/tetris.js
@@ -1,31 +1,71 @@
-var board = new Board();
+// Create 4 player windows and set first window as keydown target
+var players = [new Board(), new Board(), new Board(), new Board()];
+var activeBoard = players[0];
 
 function Board() {
+  // Instance count + ID used for DOM manipulation
+  Board.numInstances = (Board.numInstances || 0) + 1;
+  this.gameID = Board.numInstances;
+
   this.height = 22;
   this.width = 10;
-  this.activeTetramino = {};
+  this.board = [];
+  this.next = [[' ', ' ', ' ', ' '],[' ', ' ', ' ', ' '],[' ', ' ', ' ', ' ']];
 
   this.init = function() {
-    this.board = [];
-
-    //[Row][Col]
+    // Fill array with blank spaces using form [Row][Col]
     for (var h = 0; h < this.height; h++) {
       this.board.push([]);
       for (var w = 0; w < this.width; w++) {
         this.board[h].push(' ');
       }
     }
-    this.activeTetramino = new Tetramino(this)
+
+    // Add game container into DOM
+    var gameContainer = document.createElement("div");
+    gameContainer.setAttribute("id", "gameId" + this.gameID);
+    document.body.appendChild(gameContainer);
+
+    if (this.gameID === 1) {
+      var statusBar = document.createElement("div");
+      statusBar.setAttribute("id", "status");
+      document.body.appendChild(statusBar);
+
+      this.updateNext();
+    } else {
+      this.activeTetramino = new Tetramino(this);
+    }
+    gameContainer.onclick = function() { activeBoard = players[that.gameID - 1] }
+
+    this.playing = true;
     this.update();
   }
 
+  var that = this;
+  this.update = function() {
+    that.moveBlock();
+    if (that.playing) {
+      setTimeout(that.update, 500);
+    }
+  }
+
   this.display = function() {
-    document.getElementById("game").innerHTML = "";
+    document.getElementById("gameId" + this.gameID).innerHTML = "";
     for (var row = 2; row < this.height; row++) {
       var p = document.createElement("p");
       var text = document.createTextNode(this.board[row].join(''));
       p.appendChild(text);
-      document.getElementById("game").appendChild(p);
+      document.getElementById("gameId" + this.gameID).appendChild(p);
+    }
+  }
+
+  this.displayNext = function() {
+    document.getElementById("status").innerHTML = "";
+    for (var row = 0; row < this.next.length; row++) {
+      var p = document.createElement("p");
+      var text = document.createTextNode(this.next[row].join(''));
+      p.appendChild(text);
+      document.getElementById("status").appendChild(p);
     }
   }
 
@@ -41,18 +81,41 @@ function Board() {
     for (var block = 0; block < updatedPiece.length; block++) {
       var newPos = [updatedPiece[block][0] + yRow, updatedPiece[block][1] + xCol];
       if (newPos[0] === this.height) {
-        board.activeTetramino = new Tetramino(this);
+        if (this.gameID === 1) {
+          this.updateNext();
+        } else {
+          this.activeTetramino = new Tetramino(this);
+        }
         return false;
       } else if (newPos[1] < 0 || newPos[1] >= this.width) {
         return false;
-      } else if (this.board[newPos[0]][newPos[1]] === '█') {
+      } else if (this.board[newPos[0]][newPos[1]] != ' ') {
           if (direction === 'down') {
-            board.activeTetramino = new Tetramino(this);
+            if (newPos[0] <= 2) {
+              this.playing = false;
+            } else {
+              if (this.gameID === 1) {
+                this.updateNext();
+              } else {
+                this.activeTetramino = new Tetramino(this);
+              }
+            }
           }
           return false;
       }
     }
     return true;
+  }
+
+  this.updateNext = function() {
+    this.activeTetramino = this.nextTetramino || new Tetramino(this);
+    this.nextTetramino = new Tetramino(this);
+    this.next = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']];
+    this.nextTetramino.pivot.xCol = 2;
+    this.nextTetramino.blocks = this.nextTetramino.build();
+    this.addToArray(this.nextTetramino, this.next);
+    this.nextTetramino.pivot.xCol = Math.round(this.width /2);
+    this.displayNext();
   }
 
   this.lineCheck = function() {
@@ -70,22 +133,16 @@ function Board() {
     return false;
   }
 
-  this.draw = function(tetramino) {
+  this.addToArray = function(tetramino, where) {
     for (var i = 0; i < tetramino.blocks.length; i++) {
-      this.board[tetramino.blocks[i][0]][tetramino.blocks[i][1]] = '█';
+      (where || this.board)[tetramino.blocks[i][0]][tetramino.blocks[i][1]] = '█';
     }
   }
 
-  this.undraw = function(tetramino) {
+  this.removeFromArray = function(tetramino, where) {
     for (var i = 0; i < tetramino.blocks.length; i++) {
-      this.board[tetramino.blocks[i][0]][tetramino.blocks[i][1]] = ' ';
+      (where || this.board)[tetramino.blocks[i][0]][tetramino.blocks[i][1]] = ' ';
     }
-  }
-
-  var that = this;
-  this.update = function() {
-    that.moveBlock();
-    setTimeout(that.update, 500);
   }
 
   this.moveBlock = function(direction) {
@@ -97,7 +154,9 @@ function Board() {
         this.activeTetramino.updatePivot(+1, 0);
         break;
       case 'rotateClockwise':
-        this.activeTetramino.updatePivot(0, 0, 'rotateClockwise');
+        if (this.activeTetramino.shapeType != 4) {
+          this.activeTetramino.updatePivot(0, 0, 'rotateClockwise');
+        }
         break;
       default:
         this.activeTetramino.updatePivot(0, +1, 'down');
@@ -108,7 +167,7 @@ function Board() {
 };
 
 function Tetramino(board) {
-  this.arrangementType = Math.floor(Math.random() * 7) + 1;
+  this.shapeType = Math.floor(Math.random() * 7) + 1;
   this.pivot = {
     'xCol': Math.round(board.width /2),
     'yRow': 1
@@ -116,7 +175,7 @@ function Tetramino(board) {
   this.blocks = [];
 
   this.updatePivot = function(newXCol,newYRow,direction) {
-    board.undraw(this);
+    board.removeFromArray(this);
     if (board.collisionCheck(this,newXCol,newYRow,direction)) {
       this.pivot.yRow += newYRow;
       this.pivot.xCol += newXCol;
@@ -126,7 +185,7 @@ function Tetramino(board) {
     }
     this.blocks = this.build();
     if (!board.lineCheck()) {
-      board.draw(this);
+      board.addToArray(this);
     }
     board.display();
   }
@@ -136,6 +195,7 @@ function Tetramino(board) {
     switch (id) {
       case 1: // I
         return [
+          [0, 0],
           [-1, 0],
           [-2, 0],
           [+1, 0]
@@ -143,6 +203,7 @@ function Tetramino(board) {
         break;
       case 2: // T
         return [
+          [0, 0],
           [-1, 0],
           [-1, -1],
           [+1, 0]
@@ -150,6 +211,7 @@ function Tetramino(board) {
         break;
       case 3: // L
         return [
+          [0, 0],
           [-1, 0],
           [+1, 0],
           [+1, -1]
@@ -157,6 +219,7 @@ function Tetramino(board) {
         break;
       case 4: // O
         return [
+          [0, 0],
           [-1, 0],
           [-1, -1],
           [0, -1]
@@ -164,6 +227,7 @@ function Tetramino(board) {
         break;
       case 5: // J
         return [
+          [0, 0],
           [-1, 0],
           [+1, 0],
           [0, -1]
@@ -172,12 +236,14 @@ function Tetramino(board) {
       case 6: // Z
         return [
           [0, +1],
+          [0, +1],
           [-1, +1],
           [+1, 0]
         ];
         break;
       case 7: // S
         return [
+          [0, +1],
           [-1, 0],
           [0, +1],
           [+1, +1]
@@ -191,12 +257,6 @@ function Tetramino(board) {
     var x1 = 0, x2 = 1, y1 = -1, y2 = 0;
     var x = 0, y = 0;
 
-    switch(this.arrangement) {
-    case 4:
-      return this.arrangement;
-      break;
-    }
-
     return this.arrangement.map(function(block) {
       x = block[0], y = block[1];
       return [(x1 * x) + (x2 * y), (y1 * x) + (y2 * y)];
@@ -207,32 +267,32 @@ function Tetramino(board) {
     if (!arrangement) {
       arrangement = this.arrangement;
     }
-    var renderedBlock = [[this.pivot.yRow, this.pivot.xCol]];
-    for (var block = 0; block < arrangement.length; block++) {
-      renderedBlock.push([(arrangement[block][1]*-1) + this.pivot.yRow, arrangement[block][0] + this.pivot.xCol]);
+    var renderedBlock = [[this.pivot.yRow + arrangement[0][1], this.pivot.xCol]];
+    for (var block = 1; block < arrangement.length; block++) {
+      renderedBlock.push([(arrangement[block][1]*-1) + renderedBlock[0][0], arrangement[block][0] + renderedBlock[0][1]]);
     }
     return renderedBlock;
   }
-  this.arrangement = this.randomTetramino(this.arrangementType);
+  this.arrangement = this.randomTetramino(this.shapeType);
 };
 
 document.onkeydown = function(evt) {
   evt = evt || window.event;
   switch (evt.which || evt.keycode) {
     case 37: // left
-      board.moveBlock('left');
+      activeBoard.moveBlock('left');
       break;
 
     case 38: // up
-      board.moveBlock('rotateClockwise');
+      activeBoard.moveBlock('rotateClockwise');
       break;
 
     case 39: // right
-      board.moveBlock('right');
+      activeBoard.moveBlock('right');
       break;
 
     case 40: // down
-      board.moveBlock('down');
+      activeBoard.moveBlock('down');
       break;
 
     default:
